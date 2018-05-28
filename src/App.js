dafda5d4c8b475f05b6a40716ff4f72885e3e1ae
Tyler Collier <tyler.collier+github@devmounta.in>
2018-05-28 15:27:40 -0700

    Add 3 'products' (squares) and animate them

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAnimating: false,
+    };
+  }
+
   render() {
+    const { isAnimating } = this.state;
+    const productClassName = isAnimating ? "product product-animate" : "product";
+
     return (
       <div>
         <nav>
@@ -10,6 +20,12 @@ class App extends Component {
           <div className="menu phone">MENU</div>
           <div className="menu desktop">ABOUT DOWNLOAD CONTACT</div>
         </nav>
+        <div className="products">
+          <div className={productClassName}>Hello</div>
+          <div className={productClassName}>Hello</div>
+          <div className={productClassName}>Hello</div>
+        </div>
+        <button onClick={() => this.setState({ isAnimating: !this.state.isAnimating })}>Click me to animate</button>
       </div>
     );
   }
