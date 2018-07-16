7a9342cec727605dd03d89974dce28dfab906195
Tyler Collier <tyler.collier+github@devmounta.in>
2018-07-16 10:00:41 -0700

    Add slide-out menu

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAnimating: false,
+      showMenu: false,
+    };
+  }
+  
   render() {
     return (
       <div className="app">
         <nav>
           <div className="logo small">Logo</div>
           <div className="logo large">Logo</div>
-          <div className="menu small">Menu</div>
+          <div className="menu small">
+            <button onClick={() => this.setState({ showMenu: !this.state.showMenu })}>Menu</button>
+          </div>
           <div className="menu large">About Download Contact</div>
         </nav>
+        <div className={'slide-out-menu' + (this.state.showMenu ? ' show' : ' hide')}>
+          <div>About</div>
+          <div>Download</div>
+          <div>Contact</div>
+        </div>
         <div className="hero">
           <h1>Greyscale</h1>
         </div>
         <div className="animations">
-          <div className="square my-animation">
-            My text
+          <div>
+            <div className={'square' + (this.state.isAnimating ? ' my-animation' : '')}>
+              My text
+            </div>
+            <div><button onClick={() => this.setState({ isAnimating: !this.state.isAnimating })}>Toggle animation</button></div>
           </div>
           <div className="square circleify"></div>
         </div>
